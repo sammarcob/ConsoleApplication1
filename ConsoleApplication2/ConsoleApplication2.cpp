d3bb970686176159928f246ffea71babfd840752
Brooks Sammarco <sammarcob@rsd407.org>
2021-09-09 08:43:50 -0700

    THIS IS THE SECOND PROJECT

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    printf("miyoshi made me make this second code thing by right clicking on solution and clicking add");
+}
+
