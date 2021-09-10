43ecf8aede6f740d0c6d6c05b94172b112f86db2
Brooks Sammarco <sammarcob@rsd407.org>
2021-09-10 09:38:03 -0700

    update comments

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,13 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/*
+Project Name:Hello World!
+Author:Brooks Sammarco
+File Name: ConsoleApplication1.cpp
+Purpose: Intruductary code to print "Hello World" and 4 other lines
+Pseudocode: prints 5 lines saying "Hello World!", "ayeyaheyaheyaheyahe", "I am going insane", "moshroom" and "yep"
+Matinence Log:
+9/9/21: I wrote 4 lines of code not including "Hello World!"
+9/10/21: i wrote all of the comments and added a line that  prints "yep"
+*/
 #include <iostream>
 #include <conio.h>
 #include <stdio.h>
@@ -11,6 +18,6 @@ int main()
     printf("ayeyaheyaheyaheyahe\n");
     printf("I am going insane\n");
     printf("moshroom\n");
-    printf("santa naver came this christmas...\n")
+    printf("yep");
     return 0;
 }
