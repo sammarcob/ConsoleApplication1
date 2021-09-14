4a9f01825b6c34704ceb514c138988c61a1fe420
Brooks Sammarco <sammarcob@rsd407.org>
2021-09-14 10:14:17 -0700

    ints 'n stuff

---
+++ ints 'n stuff/ints 'n stuff.cpp
@@ -0,0 +1,21 @@
+//
+
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+int main()
+{
+	int i = 1;
+	printf("%i\n", i);
+	printf("%i\n", i + 1);
+	printf("%i\n", i + 2);
+	printf("%i\n", i + 3);
+	printf("%i\n", i + 4);
+	printf("%i\n", i + 5);
+	printf("%i\n", i + 6);
+	printf("%i\n", i + 7);
+	printf("%i\n", i + 8);
+	printf("%i\n", i + 9);
+	_getch();
+	return 0;
+} 
