3a33512211fe249a12671918938ca1cff096a25f
Brooks Sammarco <sammarcob@rsd407.org>
2021-09-14 11:00:02 -0700

    variables and constants 'n stuff update

---
+++ variables and constants 'n stuff/variables and constants 'n stuff.cpp
@@ -0,0 +1,20 @@
+// variables and constants 'n stuff.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int y = 5;
+	int e = 6;
+	int p = 7;
+	printf("%i\n", y);
+	printf("%i\n", e);
+	printf("%i\n", p);
+	_getch();
+
+	return 0;
+}
