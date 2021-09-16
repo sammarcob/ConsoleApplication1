787fe99f50df7a6137b285ba2bb80b5cf0bb9e4d
Brooks Sammarco <sammarcob@rsd407.org>
2021-09-16 09:04:55 -0700

    update to var and const

--- variables and constants 'n stuff/variables and constants 'n stuff.cpp
+++ variables and constants 'n stuff/variables and constants 'n stuff.cpp
@@ -5,16 +5,36 @@
 #include <string>
 #include <stdio.h>
 #include <conio.h>
+using namespace std;
+
 
 int main()
 {
 	int y = 5;
 	int e = 6;
-	int p = 7;
+	int s = 7;
 	printf("%i\n", y);
 	printf("%i\n", e);
-	printf("%i\n", p);
+	printf("%i\n", s);
 	_getch();
+	system("CLS");
+	string h = "toast";
+	string a = "alsotoast";
+	string i = "stilltoast";
+	cout<< h << endl;
+	cout<< a << endl;
+	cout<< i << endl;
+	_getch();
+	system("CLS"); 
+	double h = -3;
+	double e = -2;
+	double c = -1;
+	printf("%lf\n", h);
+	printf("%lf\n", e);
+	printf("%lf\n", c);
+	_getch();
+	system("CLS");
+	char _32 = 'x';
 
 	return 0;
 }
