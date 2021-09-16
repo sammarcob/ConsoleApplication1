bc5a4407a127b305acda1edaf3d77ba90bf2b93d
Brooks Sammarco <sammarcob@rsd407.org>
2021-09-16 08:30:09 -0700

    RPG update

--- ConsoleApplication2/RPG.cpp
+++ ConsoleApplication2/RPG.cpp
@@ -1,12 +1,39 @@
-// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+Project Name: RPG intro
+Author:Brooks Sammarco
+File Name: RPG.cpp
+Purpose: Introduction to my RPG
+Pseudocode: Prints 16 lines of code 
+Matinence Log:
+9/16/21:wrote all the lines of code and the comments
+*/
+
 
 #include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    printf("miyoshi made me make this second code thing by right clicking on solution and clicking add");
+    printf("'oh i need my Bred so i can toast it'\n");
+    _getch();
+    printf("'oh i have some Bred'\n");
+    printf("'but my toaster is over there'\n");
+    _getch();
+    printf("'now i have to go across my house to look for my toaster'\n");
+    printf("'darn this sucks'\n");
+    _getch();
+    printf("'there will probably be terrifying forks and spoons along the way maybe'\n");
+    _getch();
+    printf("'it will probably be fine'\n");
+    _getch();
+    system("CLS");
+    printf("This is an RPG by Brooks Sammarco about trying to make some toast\n");
+    printf("Hopefully it will be cool and funny\n");
+    _getch();
+    printf("And good...");
+    return 0;
 }
 
  
