3123fae5ccfc0e05efe6debfddb301ecef6b57e5
Brooks Sammarco <sammarcob@rsd407.org>
2021-09-17 08:53:49 -0700

    finish var and const

--- variables and constants 'n stuff/variables and constants 'n stuff.cpp
+++ variables and constants 'n stuff/variables and constants 'n stuff.cpp
@@ -26,15 +26,25 @@ int main()
 	cout<< i << endl;
 	_getch();
 	system("CLS"); 
-	double h = -3;
-	double e = -2;
+	double k = -3;
+	double l = -2;
 	double c = -1;
-	printf("%lf\n", h);
-	printf("%lf\n", e);
+	printf("%lf\n", k);
+	printf("%lf\n", l);
 	printf("%lf\n", c);
 	_getch();
 	system("CLS");
 	char _32 = 'x';
-
+	char _36 = 'y';
+	char _40 = 'z';
+	printf("%c\n", _32);
+	printf("%c\n", _36);
+	printf("%c\n", _40);
+	_getch();
+	system("CLS");
+	printf("this is how many toast i have:");
+	_getch();
+	bool toast = false;
+	cout << ("%i\n", toast);
 	return 0;
 }
