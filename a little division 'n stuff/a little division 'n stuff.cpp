d054af9c36c752f9824682f497c5a0ede94a60c4
Brooks Sammarco <sammarcob@rsd407.org>
2021-09-23 09:05:11 -0700

    update division

--- a little division 'n stuff/a little division 'n stuff.cpp
+++ a little division 'n stuff/a little division 'n stuff.cpp
@@ -13,32 +13,44 @@ Pseudocode:  (State your algorithm in English here.)
 
 #include <stdio.h>
 #include <conio.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	printf("addition problems\n");
+	printf("int problems:\n");
 	_getch();
 	int a = 6;
 	int b = 2;
-	int c = 8;
+	int c;
 	int d = 5;
-	int e = 8;
-	int f = 9;
+	int e = 3;
+	int f;
 	int g = 1;
 	int h = 4;
-	int i = 7;
+	int i;
 	float j = 1.1;
 	float k = 2.2;
-	float l = 3.3;
+	float l;
 	float m = 4.4;
 	float n = 5.5;
-	float o = 6.6;
+	float o;
 	float p = 7.7;
 	float q = 8.8;
-	float r = 9.9;
+	float r;
 
-	printf("a(6) + b(2) = %i\n", c);
-	printf("j(1.1) + k(2.2) = %f\n", l);
+	c = a / b;
+	printf("a/b= %i\n", c);
+	_getch();
+	f = d - e;
+	printf("d-e= %i\n", f);
+	_getch();
+	i = g + h;
+	printf("g+h= %i\n", i);
+	_getch();
+	system("CLS");
+
+
 
 	
 
