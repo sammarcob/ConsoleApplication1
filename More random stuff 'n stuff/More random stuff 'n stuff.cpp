116ff470f27ae48ce765148a2480758c49f01b7d
Brooks Sammarco <sammarcob@rsd407.org>
2021-09-30 08:58:31 -0700

    more randonm stuff

---
+++ More random stuff 'n stuff/More random stuff 'n stuff.cpp
@@ -0,0 +1,24 @@
+/*
+
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <string>
+using namespace std;
+int main()
+{
+	srand(0);
+	srand((unsigned)time(NULL));
+
+	float minfloat = 0.0;
+	float maxfloat = 1.0;
+	float f;
+	f = (float)rand() / (float)RAND_MAX;
+	printf("input a minimum number:\n");
+
+}
+
+
