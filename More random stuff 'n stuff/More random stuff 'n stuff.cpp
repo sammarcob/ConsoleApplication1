f766efd4ac2d821fb234acb5cda7ea00cb50b677
Brooks Sammarco <sammarcob@rsd407.org>
2021-10-01 09:11:51 -0700

    update

--- More random stuff 'n stuff/More random stuff 'n stuff.cpp
+++ More random stuff 'n stuff/More random stuff 'n stuff.cpp
@@ -17,8 +17,14 @@ int main()
 	float maxfloat = 1.0;
 	float f;
 	f = (float)rand() / (float)RAND_MAX;
-	printf("input a minimum number:\n");
-
+	printf("input a minimum number, then hit enter:\n");
+	cin >> minfloat;
+	fseek(stdin, 0, SEEK_END);
+	printf("input a max number, then hit enter\n");
+	cin >> maxfloat;
+	fseek(stdin, 0, SEEK_END);
+	f = minfloat + rand() % (maxfloat - minfloat);
+	printf("%f", f);
 }
 
 
