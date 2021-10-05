a2ad43ec02d129df4c84874d049aa3ebc22030db
Brooks Sammarco <sammarcob@rsd407.org>
2021-10-05 10:59:43 -0700

    update

--- Bus Charter 'n stuff/Bus Charter 'n stuff.cpp
+++ Bus Charter 'n stuff/Bus Charter 'n stuff.cpp
@@ -3,9 +3,21 @@
 */
 
 #include <iostream>
-
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+using namespace std;
 int main()
 {
+	int people;
+	int bus = 7;
+	
+	printf("input the number of people on the tour; \n");
+	cin >> people;
 
+
+	return 0;
 }
 
