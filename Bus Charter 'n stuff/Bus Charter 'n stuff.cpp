624eaf88cf3c21a5634e4499b28ddafa9734770c
Brooks Sammarco <sammarcob@rsd407.org>
2021-10-07 09:23:51 -0700

    done

--- Bus Charter 'n stuff/Bus Charter 'n stuff.cpp
+++ Bus Charter 'n stuff/Bus Charter 'n stuff.cpp
@@ -1,5 +1,13 @@
 /*
-
+Author: Brooks Sammarco
+Project Name: Bus Charter
+File Name: Bus charter 'n stuff.cpp
+Purpose: makes a bus charter to decide the ammount of busses and vans you need for a tour
+Pseudocode: writes code that asks for your name, number of people on your tour, then couculates the number of busses you need
+			for all the people, then calculates the number of vans you need for the remaining people.
+Maintenance Log:
+10/6/21: started the code (20 lines)
+10/7/21: Finished the code
 */
 
 #include <iostream>
@@ -11,12 +19,33 @@
 using namespace std;
 int main()
 {
+	int vans;
+	int vanpeeps;
 	int people;
-	int bus = 7;
-	
-	printf("input the number of people on the tour; \n");
-	cin >> people;
+	int buscap = 60;
+	int busSum;
+	string name;
+	string input;
 
+	printf("what is your first name?\n");
+	cin >> name;
+	system("CLS");
+
+
+	printf("input the number of people on the tour %s; \n", name.c_str());
+	cin >> people;
+	busSum = people / buscap;
+	printf("This is how many busses, %s: %i\n", name.c_str(), busSum);
+	
+	_getch();
+
+	vanpeeps = people % buscap;
+
+	vans = vanpeeps / 7 +1;
+	printf("This is how many vans you need for the people not on busses, %s: %i\n", name.c_str(), vans);
+	_getch();
+	system("CLS");
+	printf("you might have one more van than you need, but hey %s, it's always good to have one extra just in case :)\n", name.c_str());
 
 	return 0;
 }
