2a0927fea5f3b422004835a59baef0bb2747226c
Brooks Sammarco <sammarcob@rsd407.org>
2021-10-11 09:45:18 -0700

    update

---
+++ Averages 'n stuff/Averages 'n stuff.cpp
@@ -0,0 +1,21 @@
+/*
+
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	int Milesperhour;
+	int hoursTraveled;
+	int gasuse;
+	int 
+
+
+	return 0;
+}
