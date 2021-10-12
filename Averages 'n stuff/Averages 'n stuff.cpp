98b01bf149f186f27a6c153852bd0e512a7a1024
Brooks Sammarco <sammarcob@rsd407.org>
2021-10-12 10:22:52 -0700

    update

--- Averages 'n stuff/Averages 'n stuff.cpp
+++ Averages 'n stuff/Averages 'n stuff.cpp
@@ -11,11 +11,40 @@
 using namespace std;
 int main()
 {
-	int Milesperhour;
-	int hoursTraveled;
-	int gasuse;
-	int 
+	int MPH;
+	float hours;
+	float gas;
+	float miles;
+	float MPG;
+	string name;
 
+	printf("please input your first name:\n");
+	cin >> name;
+	system("CLS");
+
+	printf("hello %s, please input how far in miles you travled:\n", name.c_str());
+	cin >> miles;
+	printf("%f miles? ok.\n", miles);
+
+	printf("ok %s, now input how long in hours you travled:\n", name.c_str());
+	cin >> hours;
+	printf("%f hours? ok.\n", hours);
+
+	printf("ok %s, please input the ammount of fuel in gallons your car used throuhout the trip:\n", name.c_str());
+	cin >> gas;
+	printf("%f gallons? ok.\n", gas);
+
+	MPH = miles / hours;
+	MPG = miles / gas;
+
+	_getch();
+	system("CLS");
+
+	printf("ok %s, i've done the calculations: \n", name.c_str());
+	_getch();
+	printf("through the trip, you went about %i MPH\n", MPH);
+	printf("on the trip you got about %f miles for every gallon you spent\n", MPG);
+	_getch();
 
 	return 0;
 }
