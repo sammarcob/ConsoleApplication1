d3333a6a95a8694ce38fc909c9ec8adbced65cca
Brooks Sammarco <sammarcob@rsd407.org>
2021-10-19 11:54:53 -0700

    update

--- switch computer programming quiz 'n stuff/switch computer programming quiz 'n stuff.cpp
+++ switch computer programming quiz 'n stuff/switch computer programming quiz 'n stuff.cpp
@@ -1,9 +1,45 @@
-// switch computer programming quiz 'n stuff.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+
+*/
+
 
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+using namespace std;
 
 int main()
 {
-    printf
+    string name;
+    int ans1 = 0;
+    int ans2;
+    int ans3;
+    float score = 0;
+
+    printf("please input your name\n");
+    cin >> name;
+    printf("ok %s, the test will commence shortly\n", name.c_str());
+    printf("-press any button to start-");
+    _getch();
+    system("CLS");
+
+    printf("- please input the code to print a sentence in the language c++\n");
+    printf("1. say(hello);\n");
+    printf("2. printf(\"hello\")\n");
+    printf("3. tell=*hello*:\n");
+    printf("4. bozo.bozo@bozo.com       bozo");
+    cin >> ans1;
+    switch (ans1)
+    {
+    case 1:
+        printf("wrong awnser bozo\n");
+        break;
+    case 2:
+        printf("")
+    }
+
+    return 0;
 }
