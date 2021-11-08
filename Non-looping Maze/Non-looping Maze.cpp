7a03fcc87237fe7f47400f1ad1d879b984314d65
Brooks Sammarco <sammarcob@rsd407.org>
2021-11-08 09:44:30 -0800

    update

--- Non-looping Maze/Non-looping Maze.cpp
+++ Non-looping Maze/Non-looping Maze.cpp
@@ -13,49 +13,8 @@ using namespace std;
 
 int main()
 {
-    printf("press any button to shoot\n");
-    _getch();
-    printf("BANG\n");
-    Sleep(30);
-    system("CLS");
-    printf("       &\n");
-    Sleep(30); 
-    system("CLS");
-    printf("-      &\n");
-    Sleep(30); 
-    system("CLS");
-    printf(" -     &\n");
-    Sleep(30); 
-    system("CLS");
-    printf("  -    &\n");
-    Sleep(30);
-    system("CLS");
-    printf("   -   &\n");
-    Sleep(30); 
-    system("CLS");
-    printf("    -  &\n");
-    Sleep(30); 
-    system("CLS");
-    printf("     - &\n");
-    Sleep(30); 
-    system("CLS");
-    printf("      -&\n");
-    Sleep(30); 
-    system("CLS");
-    printf("      (x)\n");
-    Sleep(10);
-    system("CLS");
-    printf("     (- x -)\n");
-    Sleep(10);
-    system("CLS");
-    printf("    (-  x  -)\n");
-    Sleep(10);
-    system("CLS");
-    printf("   (-   x   -)  \n");
-    Sleep(10);
-    system("CLS");
-    printf("              ")
-    _getch();
+    
+ 
 
     return 0;
 }
