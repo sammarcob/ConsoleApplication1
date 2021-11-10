a95e3749a0e47317c1c697a75e32135697000760
Brooks Sammarco <sammarcob@rsd407.org>
2021-11-10 09:46:04 -0800

    update

--- Non-looping Maze/Non-looping Maze.cpp
+++ Non-looping Maze/Non-looping Maze.cpp
@@ -13,8 +13,39 @@ using namespace std;
 
 int main()
 {
-    
+    string name;
+    string decision;
+
+    printf("welcome to the maaaazzzeee\n");
+    printf("enter your name to enter the maaaaazzzeee\n");
+    cin >> name;
+    system("CLS");
+    printf("ok, %s", name.c_str());
+    printf("-press any key to continue-");
+    _getch();
+    system("CLS");
+
+    printf("1. right\n2. down\n");
+    cin >> decision;
+    if (decision == "right")
+    {
+        system("CLS");
+        printf("1. right\n2. down\n");
+        cin >> decision;
+        if (decision == "right")
+        {
+            printf("you reached a dead end");
+            return 0;
+        }
+    }
+    else if (decision == "down")
+    {
+        system("CLS");
+        printf("1. right\n2. down\n");
+        cin >> decision;
+    }
+
+
  
 
     return 0;
-}
