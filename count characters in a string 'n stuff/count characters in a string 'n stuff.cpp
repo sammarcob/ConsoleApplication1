2b9dc94cc3fa13c2e22091d7a0fd8aa3dbef408f
Brooks Sammarco <sammarcob@rsd407.org>
2021-11-30 11:01:01 -0800

    update

---
+++ count characters in a string 'n stuff/count characters in a string 'n stuff.cpp
@@ -0,0 +1,40 @@
+/*
+
+*/
+
+#include <iostream>
+#include <stdlib.h> 
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <windows.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	string text;
+	int spaces = 0;
+	int newline = 0;
+	int numbers = 0;
+	int tabs = 0;
+
+	printf("enter a sentence.\n");
+	printf("press the tild key, then enter when you are done\n");
+	cin >> text;
+	getline(cin, text, '~');
+
+	printf("your text is %s characters long", text.length());
+
+	for (int i = 0; i < text.length(); i++)
+	{
+		if (text[i] == '')
+		{
+
+
+
+		}
+	}???
+	return 0;
+}
