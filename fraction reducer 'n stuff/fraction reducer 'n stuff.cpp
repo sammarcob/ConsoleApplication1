ff8be73177884fbb5c898d2e0703eee288a8042a
Brooks Sammarco <sammarcob@rsd407.org>
2022-01-05 11:03:16 -0800

    update

---
+++ fraction reducer 'n stuff/fraction reducer 'n stuff.cpp
@@ -0,0 +1,26 @@
+
+
+#include <iostream>
+#include <stdlib.h> 
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <windows.h>
+using namespace std;
+
+int GCF(int num, int den);
+
+int main()
+{
+	int num;
+	int den;
+	
+
+	printf("enter the fraction you want to reduce:\n");
+}
+
+bool isPrime()
+{
+	return false;
+}
