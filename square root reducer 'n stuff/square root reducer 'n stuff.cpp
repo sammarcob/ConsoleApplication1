e7f30ab44b3f50da93780755d7595ee383eeb71b
Brooks Sammarco <sammarcob@rsd407.org>
2022-01-06 09:45:16 -0800

    update

---
+++ square root reducer 'n stuff/square root reducer 'n stuff.cpp
@@ -0,0 +1,29 @@
+/*
+Author: Brooks Sammarco
+pair programming partner: Slater Swart
+Project Name: 
+File Name: 
+Purpose: 
+Pseudocode: 
+Maintenance Log:
+
+*/
+
+#include <iostream>
+#include <stdlib.h> 
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <windows.h>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+
+}
+trail you suck 
+
+like bals
