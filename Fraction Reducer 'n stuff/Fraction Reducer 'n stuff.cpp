a03d9415914ddcae6a29a0cc6e1ee8be8b9beef4
Brooks Sammarco <90403870+sammarcob@users.noreply.github.com>
2022-01-20 17:34:14 -0800

    bruh

---
+++ Fraction Reducer 'n stuff/Fraction Reducer 'n stuff.cpp
@@ -0,0 +1,40 @@
+
+
+#include <iostream>
+#include <stdlib.h> 
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <windows.h>
+#include <vector>
+using namespace std;
+
+bool isPrime(int n)
+{
+    int n = true;
+    printf("this is in prime\n");
+    return true;
+}
+
+int main()
+{
+    int numer = 0;
+    int denom = 0;
+    printf("please input your numerator\n");
+    cin >> numer;
+    printf("please input your denominator\n");
+    cin >> denom;
+    printf("numerator: %i , denomenator: %i\n", numer, denom);
+    cout << "bruh" << endl;
+
+    /*
+    while (!isPrime(numer))
+    {
+
+
+
+    }
+    */
+    return 0;
+}
