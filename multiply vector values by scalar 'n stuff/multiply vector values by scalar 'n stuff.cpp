44ca60aba80323aec10fc513ca5db5d35af6ed55
Brooks Sammarco <sammarcob@rsd407.org>
2022-01-31 09:40:03 -0800

    update

---
+++ multiply vector values by scalar 'n stuff/multiply vector values by scalar 'n stuff.cpp
@@ -0,0 +1,17 @@
+
+
+#include <stdlib.h> 
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <windows.h>
+#include <vector>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    
+}
