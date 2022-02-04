4d5b7686d5cbf26474b1fdac866e8ab7267f39b2
Brooks Sammarco <sammarcob@rsd407.org>
2022-02-04 09:06:09 -0800

    update

--- multiplication table flash cards 'n stuff/multiplication table flash cards 'n stuff.cpp
+++ multiplication table flash cards 'n stuff/multiplication table flash cards 'n stuff.cpp
@@ -5,7 +5,8 @@ File Name: multiplication table flash cards 'n stuff.cpp
 Purpose: quizzes you on your multiplication tables
 Pseudocode: 
 Maintenance Log:
-2/3/22: 
+2/3/22: wrote the for loops
+2/4/22: 
 */
 
 #include <iostream>
@@ -21,15 +22,62 @@ using namespace std;
 
 int main()
 {
+    srand(0);
+    srand((unsigned)time(NULL));
+    string choice;
+    int ans;
+
     vector<vector<int>>numbers;
-    for (int i = 1; i < 12; i++)
+    for (int i = 1; i <= 12; i++)
     {
         vector<int>temp;
-        for (int f = 1; f < 12; f++)
+        for (int f = 1; f <= 12; f++)
         {
-            temp.push_back(f);
+            temp.push_back(f * i);
         }
+        numbers.push_back(temp);
     }
+    
+    
+    printf("do you want a flash card?\n(y/n)");
+    cin >> choice;
+    if (choice == "y")
+    {
+       while (true)
+       {
+                int wer = 1 + (rand() % 12);
+                int qwerty = 1 + (rand() % 12);
 
+                printf("%d * %d\n", wer, qwerty);
+                cin >> ans;
+
+                if (ans == numbers[wer][qwerty])
+                {
+                    printf("good job\n do you want another?\n(y/n)\n");
+                    cin >> choice;
+                    if (choice == "y")
+                    {
+                        continue;
+                    }
+                    else if (choice == "n")
+                    {
+                        return 0;
+                    }
+                }
+                else (ans != numbers[wer][qwerty])
+                {
+                    printf("wrong awnser\n");
+                    _getch();
+                    continue;
+                }
+
+       }
+    }
+    else if (choice == "n")
+    {
+        printf("why did you run this?\n");
+        _getch();
+        return 0;
+    }
     return 0;
 }
