900f7b38b9165c64ed8e450f97e48e3b7d603e00
Brooks Sammarco <sammarcob@rsd407.org>
2022-02-14 09:39:49 -0800

    update

---
+++ count characters in a file 'n stuff/count characters in a file 'n stuff.cpp
@@ -0,0 +1,21 @@
+
+
+#define _USE_MATH_DEFINES
+#include <stdlib.h> 
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <windows.h>
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	
+
+}
--- matrix multiplication 'n stuff/matrix multiplication 'n stuff.cpp
+++ matrix multiplication 'n stuff/matrix multiplication 'n stuff.cpp
@@ -36,6 +36,7 @@ int main()
             {
                 temp[i] += a[f][r] * b[i][r];
             }
+            c.push_back(temp); // maybe?
         }
     }
 
