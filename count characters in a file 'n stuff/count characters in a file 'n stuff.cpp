30e42b9ba9981c9a9201cacad5b7c106bfa3ec52
Brooks Sammarco <sammarcob@rsd407.org>
2022-02-15 10:54:42 -0800

    update

--- count characters in a file 'n stuff/count characters in a file 'n stuff.cpp
+++ count characters in a file 'n stuff/count characters in a file 'n stuff.cpp
@@ -16,6 +16,48 @@ using namespace std;
 
 int main()
 {
+	string text;
+	int spaces = 0;
+	int newline = 0;
+	int numbers = 0;
+	int tabs = 0;
+	ifstream in;
+	ofstream out("OutputFile.txt");
+	in.open("InputFile.txt");
+	in.close();
+	string temp;
+	string input;
+
 	
+
+
+	while (getline(in, temp))
+	{
+		for (int i = 0; i < temp.length(); i++)
+		{
+			if (text[i] == ' ')
+			{
+				spaces++;
+			}
+			else if (text[i] == '\t')
+			{
+				tabs++;
+			}
+			else if (text[i] == '\n')
+			{
+				newline++;
+			}
+			else if (text[i] >= '0' && text[i] <= '9')
+			{
+				numbers++;
+			}
+		}
+		
+	}
+	
+
+	_getch();
+	return 0;
+
 
 }
