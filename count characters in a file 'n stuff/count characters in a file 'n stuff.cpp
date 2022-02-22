0010ad0c1f5f79e9f4b21f0ae0c8a5892b5c0ab8
Brooks Sammarco <sammarcob@rsd407.org>
2022-02-22 10:53:40 -0800

    update

--- count characters in a file 'n stuff/count characters in a file 'n stuff.cpp
+++ count characters in a file 'n stuff/count characters in a file 'n stuff.cpp
@@ -1,30 +1,42 @@
-
+/*
+Author: Brooks Sammarco
+Project Name: count characters in a file
+File Name: count characters in a file 'n stuff.cpp
+Purpose: write code that counts the number of spaces, tabs, newlines and numbers and length in a file using getline
+Pseudocode: puts a file into a getline, then using if statements, counts the number of different things in the string
+Maintenance Log:
+2/22/21: finish
+*/
 
 #define _USE_MATH_DEFINES
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <time.h>
 #include <conio.h>
 #include <string>
 #include <stdio.h>
-#include <windows.h>
 #include <vector>
 #include <iostream>
-#include <math.h>
 #include <fstream>
 
 using namespace std;
 
 int main()
 {
-	string text;
 	int spaces = 0;
 	int newline = 0;
 	int numbers = 0;
 	int tabs = 0;
+	int length = 0;
+
 	ifstream in;
 	ofstream out("OutputFile.txt");
 	in.open("InputFile.txt");
-	in.close();
+	
+	if (!in.is_open())
+	{
+		printf("File not found");
+	}
+
 	string temp;
 	string input;
 
@@ -35,29 +47,30 @@ int main()
 	{
 		for (int i = 0; i < temp.length(); i++)
 		{
-			if (text[i] == ' ')
+			if (temp[i] == ' ')
 			{
 				spaces++;
 			}
-			else if (text[i] == '\t')
+			else if (temp[i] == '\t')
 			{
 				tabs++;
 			}
-			else if (text[i] == '\n')
-			{
-				newline++;
-			}
-			else if (text[i] >= '0' && text[i] <= '9')
+			else if (temp[i] >= '0' && temp[i] <= '9')
 			{
 				numbers++;
 			}
 		}
+		newline++;
+		length += temp.length();
 		
 	}
+	in.close();
+	cout << "spaces: " << spaces << endl;
+	cout << "tabs: " << tabs << endl;
+	cout << "lines: " << newline << endl;
+	cout << "numbers: " << numbers << endl;
+	cout << "length: " << length << endl;
 	
-
 	_getch();
 	return 0;
-
-
 }
---
+++ delimited files/delimited files.cpp
@@ -0,0 +1,25 @@
+/*
+Author: Brooks Sammarco
+Project Name: count characters in a string
+File Name: count characters in a string 'n stuff.cpp
+Purpose: write code that counts the number of spaces, tabs, newlines and numbers in a sentence
+Pseudocode: asks you to enter a sentence followed by ~ and enter, then goes down a for statement checking for certain things.
+Maintenance Log:
+2/22/22: 
+*/
+
+#define _USE_MATH_DEFINES
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    
+}
