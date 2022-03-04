6777f712a9bdd097617e79295d1f431b00e3ba5f
Brooks Sammarco <sammarcob@rsd407.org>
2022-03-04 09:06:23 -0800

    updat

--- delimited files/delimited files.cpp
+++ delimited files/delimited files.cpp
@@ -21,16 +21,28 @@ using namespace std;
 
 int main()
 {
+    string temp;
     ifstream comma;
     ofstream tab;
     comma.open("commaDelimited.txt");
-    tab.open("commaDelimited.txt");
+    tab.open("tabDelimited.txt");
 
     if (!comma.is_open())
     {
         printf("comma file not found");
     }
 
+    while (getline(comma, temp))
+    {
+        for (int i = 0; i < temp.length(); i++)
+        {
+            if (temp[i] == ',')
+            {
+                temp[i] = '\t';
+            }
+        }
+        tab << temp << "\n";
+    }
     
 
 }
