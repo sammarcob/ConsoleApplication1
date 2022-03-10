bf2f3f21adb652e3816e5cbfdd243e86888b2184
Brooks Sammarco <sammarcob@rsd407.org>
2022-03-10 08:56:31 -0800

    update

--- search a vector 'n stuff/search a vector 'n stuff.cpp
+++ search a vector 'n stuff/search a vector 'n stuff.cpp
@@ -11,50 +11,37 @@ using namespace std;
 
 struct info
 {
-    int i;
-    float f;
-    string s;
-    char c;
+    string name;
+    int age;
+    int height;
+    int weight;
+    string phone;
 };
 
 int main()
 {
+    string input;
+    ifstream search;
+    search.open("vecSearch.txt");
+
+    if (!search.is_open())
+    {
+        printf("file not found");
+    }
+
     vector <info> vec;
 
-    if (!comma.is_open())
+    while (getline(search))
     {
-        printf("comma file not found");
+        
     }
 
-    while (getline(comma, temp, ','))
+
+    if (vec[i].name == input)
     {
-        info x;
-        // all boned
-        x.i = stoi(temp);
-        tab << temp << "\t";
-
-        getline(comma, temp, ',');
-
-        x.f = stof(temp);
-        tab << temp << "\t";
-
-        getline(comma, temp, ',');
-
-        x.s = temp;
-        tab << temp << "\t";
-
-        getline(comma, temp);
-
-        x.c = temp[0];
-        tab << temp << "\n";
-
-        vec.push_back(x);
+        cout << vec[i].name;
     }
-    int respond;
-    printf("what would you like to search?\nAge: 1\nHeight: 2\nWeight: 3\nPhone Number: 4\n");
-    cin >> respond;
 
-    if (respond == 1)
 
 
     return 0;
