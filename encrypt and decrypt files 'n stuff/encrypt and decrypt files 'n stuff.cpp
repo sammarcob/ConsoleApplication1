c6f0922b6b5a6d62da90bca7f9cab8a97e8bdb3b
Brooks Sammarco <90403870+sammarcob@users.noreply.github.com>
2022-03-13 23:44:28 -0700

    update

---
+++ encrypt and decrypt files 'n stuff/encrypt and decrypt files 'n stuff.cpp
@@ -0,0 +1,71 @@
+
+#include <stdlib.h> 
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <windows.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#define _USE_MATH_DEFINE
+using namespace std;
+
+int main()
+{
+    string input;
+    string oText;
+    string eText;
+    string dText;
+    int incryptionFactor;
+    
+    ifstream text;
+    ofstream enText("encrypted text.txt");
+    text.open("textt.txt");
+
+    printf("what would you like to encrypt by: ");
+    cin >> incryptionFactor;
+    system("CLS");
+
+    while (getline(text, oText))
+    {
+
+        for (int i = 0; i < oText.length(); i++)
+        {
+            int askiiValue = (int)oText[i] + incryptionFactor;
+
+            eText += (char)askiiValue;
+        }
+        enText << eText << '\n';
+        cout << "encrypted: " << eText << endl;
+        eText = "";
+    }
+    
+    enText.close();
+    _getch();
+    system("CLS");
+    
+    ifstream encText;
+    ofstream decText("decrypted text.txt");
+    encText.open("encrypted text.txt");
+
+    
+    while (getline(encText, input))
+    {
+        for (int i = 0; i < input.length(); i++)
+        {
+            int askiiValue = (int)input[i] - incryptionFactor;
+
+            dText += (char)askiiValue;
+        }
+        decText << dText << '\n';
+        cout << dText << endl;
+        dText = "";
+    }
+    _getch();
+
+
+
+    return 0;
+}
--- search a vector 'n stuff/search a vector 'n stuff.cpp
+++ search a vector 'n stuff/search a vector 'n stuff.cpp
@@ -1,12 +1,14 @@
-#define _USE_MATH_DEFINES
-#include <stdlib.h>
+#include <stdlib.h> 
 #include <time.h>
 #include <conio.h>
 #include <string>
 #include <stdio.h>
+#include <windows.h>
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <math.h>
+#define _USE_MATH_DEFINE
 using namespace std;
 
 struct info
@@ -20,7 +22,10 @@ struct info
 
 int main()
 {
+
+    string fileInput;
     string input;
+    
     ifstream search;
     search.open("vecSearch.txt");
 
@@ -31,18 +36,152 @@ int main()
 
     vector <info> vec;
 
-    while (getline(search))
+    while (getline(search, fileInput, '\t'))
     {
-        
+        info mainInfo;
+        mainInfo.name = fileInput;
+
+        getline(search, fileInput, '\t');
+        mainInfo.age = stoi(fileInput);
+
+        getline(search, fileInput, '\t');
+        mainInfo.height = stoi(fileInput);
+
+        getline(search, fileInput, '\t');
+        mainInfo.weight = stoi(fileInput);
+
+        getline(search, fileInput, '\n');
+        mainInfo.phone = fileInput;
+
+        vec.push_back(mainInfo);
     }
-
-
-    if (vec[i].name == input)
+    while (true)
     {
-        cout << vec[i].name;
+        printf("what would you like to search by?\n\nn = name\na = age\nh = height\nw = weight\np = phone\n");
+        cin >> input;
+        if (input == "n")
+        {
+            printf("what is your name: ");
+            cin >> input;
+            bool foundResult = false;
+            for (int i = 0; i < vec.size(); i++)
+            {
+                if (vec[i].name == input)
+                {
+                    foundResult = true;
+                    cout << "name: " << vec[i].name;
+                    cout << "\tAge: " << vec[i].age;
+                    cout << "\tHeight: " << vec[i].height;
+                    cout << "\tWeight: " << vec[i].weight;
+                    cout << "\tPhone #: " << vec[i].phone;
+                }
+            }
+            if (!foundResult)
+            {
+                printf("no result found");
+            }
+        }
+        if (input == "a")
+        {
+            printf("what is your age: ");
+            cin >> input;
+            bool foundResult = false;
+            for (int i = 0; i < vec.size(); i++)
+            {
+                
+                if (vec[i].age == stoi(input))
+                {
+                    foundResult = true;
+                    cout << "name: " << vec[i].name;
+                    cout << "\tAge: " << vec[i].age;
+                    cout << "\tHeight: " << vec[i].height;
+                    cout << "\tWeight: " << vec[i].weight;
+                    cout << "\tPhone #: " << vec[i].phone;
+                }
+            }
+            if (!foundResult)
+            {
+                printf("no result found");
+            }
+        }
+        if (input == "h")
+        {
+            printf("what is your height: ");
+            cin >> input;
+            bool foundResult = false;
+            for (int i = 0; i < vec.size(); i++)
+            {
+                if (vec[i].height == stoi(input))
+                {
+                    foundResult = true;
+                    cout << "name: " << vec[i].name;
+                    cout << "\tAge: " << vec[i].age;
+                    cout << "\tHeight: " << vec[i].height;
+                    cout << "\tWeight: " << vec[i].weight;
+                    cout << "\tPhone #: " << vec[i].phone;
+                }
+            }
+            if (!foundResult)
+            {
+                printf("no result found");
+            }
+        }
+        if (input == "w")
+        {
+            printf("what is your weight: ");
+            cin >> input;
+            bool foundResult = false;
+            for (int i = 0; i < vec.size(); i++)
+            {
+                if (vec[i].weight == stoi(input))
+                {
+                    foundResult = true;
+                    cout << "name: " << vec[i].name;
+                    cout << "\tAge: " << vec[i].age;
+                    cout << "\tHeight: " << vec[i].height;
+                    cout << "\tWeight: " << vec[i].weight;
+                    cout << "\tPhone #: " << vec[i].phone;
+                }
+            }
+            if (!foundResult)
+            {
+                printf("no result found");
+            }
+        }
+        if (input == "p")
+        {
+            printf("what is your phone number: ");
+            cin >> input;
+            bool foundResult = false;
+            for (int i = 0; i < vec.size(); i++)
+            {
+                if (vec[i].phone == input)
+                {
+                    foundResult = true;
+                    cout << "name: " << vec[i].name;
+                    cout << "\tAge: " << vec[i].age;
+                    cout << "\tHeight: " << vec[i].height;
+                    cout << "\tWeight: " << vec[i].weight;
+                    cout << "\tPhone #: " << vec[i].phone;
+                }
+            }
+            if (!foundResult)
+            {
+                printf("no result found");
+            }
+        }
+        printf("\nwould you like to enter another?\n(y/n)\n");
+        cin >> input;
+        if (input == "y")
+        {
+            system("CLS");
+            continue;
+        }
+        else if (input == "n")
+        {
+            break;
+        }
     }
-
-
 
     return 0;
 }
