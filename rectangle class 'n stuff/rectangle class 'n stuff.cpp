d534530786680fb6c12b3f25a63a95ce64094d2f
Brooks Sammarco <sammarcob@rsd407.org>
2022-03-15 10:55:11 -0700

    update

---
+++ rectangle class 'n stuff/rectangle class 'n stuff.cpp
@@ -0,0 +1,17 @@
+
+#pragma once
+#define _USE_MATH_DEFINES
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    
+}
