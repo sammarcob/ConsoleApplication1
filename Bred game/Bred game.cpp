25ac2a20d03f1313294272d8d52ee3a2c683612a
Brooks Sammarco <sammarcob@rsd407.org>
2022-03-28 09:40:00 -0700

    update

---
+++ Bred game/Bred game.cpp
@@ -0,0 +1,28 @@
+/*
+Project Name: Bred game
+Author: Brooks Sammarco
+File Name: Bred game.cpp
+Purpose: plays a game that has a lot of bred in it
+Pseudocode: bred
+Matinence Log:
+3/28/22: restarted
+*/
+
+
+#pragma once
+#define _USE_MATH_DEFINES
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "Functions.cpp"
+using namespace std;
+
+int main()
+{
+    
+}
---
+++ Bred game/npc.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class npc
+{
+public:
+	npc();
+
+private:
+
+};
