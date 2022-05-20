e48fe50adc004ec0fda8a32a220891db4f2bf7ae
Brooks Sammarco <sammarcob@rsd407.org>
2022-05-20 08:57:24 -0700

    update

--- Bred game/Bred game.cpp
+++ Bred game/Bred game.cpp
@@ -33,29 +33,26 @@ int main()
 
     npcData nd;
 
-    /*npc Jelly;
-    Jelly.setData("Jelly", 7, 5);
-    Jelly.addPhrase("...");
+    npc temp;
 
-    npc Jam;
-    Jam.setData("Jam", 8, 4);
-    Jam.addPhrase("yoo what's up man wana go surfing?");
+    for (int i = 0; i < 6; i++)
+    {
+        globeVar.npcs.push_back(temp);
+    }
 
-    npc Butter;
-    Butter.setData("Butter", 4, 2);
-    Butter.addPhrase("HEY HEY HEY HEY YO YO YOOOOOOOO WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-
-    npc Chad;
-    Chad.setData("Chad", 10, 6);
-    Chad.addPhrase("dude. what's up dude. hey dude why are you looking at me like that dude?");
-
-    npc Molassas;
-    Molassas.setData("Molassas", 15, 3);
-    Molassas.addPhrase("h-h-h-h-h-h-h-e-e-e-e-e-e-e-e-e-e-y-y-y-y-y-y-y-y-y...        m-m-m-m-m-m-m-m-m-a-a-a-a-a-n-n-n-n-n-n...");
-
-    npc peanutButter;
-    peanutButter.setData("Peanut Butter", 20, 0);
-    peanutButter.addPhrase("hello my child.");*/
+    globeVar.npcs[0].setData("Jelly", 7, 5);
+    globeVar.npcs[0].addPhrase("...");
+    globeVar.npcs[1].setData("Jam", 8, 4);
+    globeVar.npcs[1].addPhrase("yoo what's up man wana go surfing?");
+    globeVar.npcs[2].setData("Butter", 4, 2);
+    globeVar.npcs[2].addPhrase("HEY HEY HEY HEY YO YO YOOOOOOOO WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+    globeVar.npcs[3].setData("Chad", 10, 6);
+    globeVar.npcs[3].addPhrase("dude. what's up dude. hey dude why are you looking at me like that dude?");
+    globeVar.npcs[4].setData("Molassas", 15, 3);
+    globeVar.npcs[4].addPhrase("h-h-h-h-h-h-h-e-e-e-e-e-e-e-e-e-e-y-y-y-y-y-y-y-y-y...        m-m-m-m-m-m-m-m-m-a-a-a-a-a-n-n-n-n-n-n...");
+    globeVar.npcs[5].setData("Peanut Butter", 20, 0);
+    globeVar.npcs[5].addPhrase("hello my child.");
+    
 
     GameManager(globeVar);
     
--- Bred game/player.cpp
+++ Bred game/player.cpp
@@ -5,7 +5,9 @@ using namespace std;
 #include "player.h"
 #include "npc.h"
 #include "nd.h"
+#include "Functions.h"
 
+globalVariables globVar;
 
 int player::damageEnemy(npc& npcDamaged)
 {
@@ -13,5 +15,5 @@ int player::damageEnemy(npc& npcDamaged)
 }
 int player::takeDamage(npc& playerDamaged)
 {
-	
+	globVar.npcs
 }
